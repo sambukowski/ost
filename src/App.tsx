3f539f0699fb09c3721a7fbf7611f1cbd6c8ef3d
Sam Bukowski <sam.c.bukowski@gmail.com>
2022-06-24 00:24:09 -0600

    added title. intial formating for save-load buttons

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function RenderGlobalClock(props: {
   setPlaying: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   return (
-    <div>
+    <div style={{ flex: 0.33 }}>
       <div className="ost_tracking_element" id="ost_global_clock">
         {/* render the clock with the formate hh:mm:ss */}
         {new Date(props.time * 1000).toISOString().substring(11, 19)}
@@ -144,6 +144,7 @@ function UpdateOSIdata(
   const newApps = [...on_screen_item.appearances];
 
   // TODO: think about this logic a little more
+  // need to add something that handles empty lists of appearances
   if (!last.end) newApps[lastIndex] = { ...last, end: time };
   else if (last.end === time) newApps[lastIndex] = { ...last, end: undefined };
   else newApps.push({ start: time });
@@ -154,6 +155,72 @@ function UpdateOSIdata(
   };
 
   setChars(newChars);
+}
+
+function MediaTitle(props: {
+  title: string;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  return (
+    <div
+      className="ost_tracking_element"
+      id="ost_media_title"
+      style={{ flex: 0.33 }}
+    >
+      <input
+        style={{ flex: 1 }}
+        className="media_title"
+        type="text"
+        value={props.title}
+        onChange={(e) => {
+          let newTitle = props.title;
+          newTitle = e.target.value;
+          props.setTitle(newTitle);
+        }}
+      />
+    </div>
+  );
+}
+
+function SaveLoad(props: {
+  title: string;
+  chars: OnScreenItem[];
+  setFiles: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  // onChange handler for loading in json data
+  const Upload = (e: any) => {
+    const fileReader = new FileReader();
+    fileReader.readAsText(e.target.files[0], "UTF-8");
+    fileReader.onload = (e: any) => {
+      props.setFiles(e.target.result);
+    };
+  };
+  return (
+    <div style={{ display: "flex" }}>
+      <button
+        className="ost_tracking_element"
+        style={{ flex: 1 }}
+        onClick={() =>
+          download(
+            props.title.replace(/\s/g, "-") + ".json",
+            JSON.stringify(props.chars, null, 2)
+          )
+        }
+      >
+        Save
+      </button>
+      <label htmlFor="loadOSTfile" className="ost_tracking_element">
+        <input
+          type="file"
+          id="loadOSTfile"
+          name="loadOSTfile"
+          placeholder="Load"
+          onChange={Upload}
+        />
+        <div>Load</div>
+      </label>
+    </div>
+  );
 }
 
 function RenderOnScreenItem(props: {
@@ -237,6 +304,7 @@ export default function App() {
   const [time, setTime] = useState(200),
     [playing, setPlaying] = useState(false),
     [files, setFiles] = useState(""),
+    [title, setTitle] = useState("Title"),
     [chars, setChars] = useState<OnScreenItem[]>([
       {
         name: "Billy",
@@ -261,16 +329,22 @@ export default function App() {
         ],
         color: "orange",
       },
+      {
+        name: "empty",
+        appearances: [],
+        color: "pink",
+      },
     ]);
-
-  // on change handler for loading in json data
-  const Upload = (e: any) => {
-    const fileReader = new FileReader();
-    fileReader.readAsText(e.target.files[0], "UTF-8");
-    fileReader.onload = (e: any) => {
-      setFiles(e.target.result);
-    };
-  };
+  // TODO: set up the default chars
+  // TODO: set up a data structure for the whole ost project
+  //      - title, length, clock value
+  // TODO: enhance the export
+  // TODO: make the file loading more robust
+  //      - error checking for input file
+  // TODO: color picker for OSIs
+  //      - make this a block on the far left, click to pull up a color picker
+  // TODO: ost timeline image exporter
+  // TODO: SVG open eye and closing eye for visability buttons
 
   // update the global clock
   useEffect(() => {
@@ -283,17 +357,15 @@ export default function App() {
   return (
     <div>
       <h1>On Screen Timer</h1>
-      <RenderGlobalClock
-        playing={playing}
-        time={time}
-        setPlaying={setPlaying}
-      />
-      <button
-        onClick={() => download("test.json", JSON.stringify(chars, null, 2))}
-      >
-        download
-      </button>
-      <input type="file" onChange={Upload} />
+      <div style={{ display: "flex" }}>
+        <RenderGlobalClock
+          playing={playing}
+          time={time}
+          setPlaying={setPlaying}
+        />
+        <MediaTitle title={title} setTitle={setTitle} />
+        <SaveLoad title={title} chars={chars} setFiles={setFiles} />
+      </div>
       {chars.map((char, i) => (
         <RenderOnScreenItem
           key={i}
@@ -317,6 +389,7 @@ export default function App() {
       >
         -
       </div>
+      <div>{title}</div>
       <div>{files}</div>
 
       <div>
